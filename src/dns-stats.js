57be0d9c364acf3349d30315298eb7eac6ab0a6b
Vladimir Fedzkin <Vovanstik@gmail.com>
2025-01-06 23:50:29 +0300

    feat: getDNSStats complite

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let current = '';
+
+    for (const part of parts) {
+      current += `.${part}`;
+      dnsStats[current] = (dnsStats[current] || 0) + 1;
+    }
+  }
+
+  return dnsStats;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
